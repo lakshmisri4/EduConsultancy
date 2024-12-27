2772bb9a3579a8f72c028b54ad0087a044694282
Lakshmi Sri <144507434+lakshmisri4@users.noreply.github.com>
2024-12-27 11:42:55 +0530

    commit

---
+++ backend/blog-service/src/main/java/com/pol/blog_service/dto/tags/TagRequestDTO.java
@@ -0,0 +1,14 @@
+package com.pol.blog_service.dto.tags;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+
+@Data
+@Builder
+public class TagRequestDTO {
+    @NotBlank(message = "Please provide a tag name")
+    private String tagName;
+}
